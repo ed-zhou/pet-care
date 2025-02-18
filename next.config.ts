ada5b476312ed15eca9ea09a5d33834b443d2737
xiaohan <zhoujh_dev@163.com>
2025-02-18 10:38:16 +0800

    提交腾讯云修改意见

--- next.config.ts
+++ next.config.ts
@@ -2,6 +2,10 @@ import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
   /* config options here */
+  serverRuntimeConfig:{
+    host: "0.0.0.0",
+    port: 9000
+  }
 };
 
 export default nextConfig;
